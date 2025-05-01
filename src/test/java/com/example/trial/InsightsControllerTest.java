e51d48adb85231aa878fafd5bbe7f57314049192
Anantha-web999 <appusananthakrishnan@gmail.com>
2025-05-01 13:04:19 +1000

    Added TDD and test cases

---
+++ src/test/java/com/example/trial/InsightsControllerTest.java
@@ -0,0 +1,16 @@
+package com.example.trial;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InsightsControllerTest {
+
+    @Test
+    void testControllerInstantiation() {
+        InsightsController controller = new InsightsController();
+        assertNotNull(controller);
+    }
+
+
+}
